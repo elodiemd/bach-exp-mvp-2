4d44516406a9eecbd65acca353bb2154e6d1bec2
Elodie Duffau <elodie.duffau@gmail.com>
2022-01-27 14:25:11 +0100

    CRUID routes before ressources

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,25 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get '/parties', to: 'parties#new' #as:thenameofyouwant
+  # CREATING CRUD FOR PARTIES
+  # read all parties : get '/parties', to: 'parties#index'
+  # read one party
+  get '/parties/:id', to: 'parties#show'
+  # create one party
+  get '/parties/new', to: 'parties#new' #as:thenameofyouwant
+  post '/parties/new', to: 'parties#create' #as:thenameofyouwant
+
+  # update one party
+  get '/parties/:id/edit', to: 'parties#edit' #as:thenameofyouwant
+  patch '/parties/:id', to: 'parties/update'
+
+  # delete one party
+  delete '/parties/:id', to: 'parties#destroy' #as:thenameofyouwant
+
+
+  # CREATING CRUD FOR DASHBOARD
   get '/dashboards', to: 'dashboards#show'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
