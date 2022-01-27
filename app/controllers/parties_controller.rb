ec27e6870dd425e3a88aa86de3aa34b2a00ee267
Elodie Duffau <elodie.duffau@gmail.com>
2022-01-27 14:42:02 +0100

    updating controller for parties

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,8 +2,27 @@ class PartiesController < ApplicationController
 
   skip_before_action :authenticate_user!, only: [ :new, :form ]
 
+  def index
+  end
+
+  def show
+  end
+
   def new
   end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 
   # def form
   #   @names= ["Bach of Elo", "Bach of Cam"]
--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  # CREATING CRUD FOR PARTIES
-  # read all parties : get '/parties', to: 'parties#index'
-  # read one party
-  get '/parties/:id', to: 'parties#show'
-  # create one party
-  get '/parties/new', to: 'parties#new' #as:thenameofyouwant
-  post '/parties/new', to: 'parties#create' #as:thenameofyouwant
+  resources :parties
+  # # CREATING CRUD FOR PARTIES
+  # # read all parties : get '/parties', to: 'parties#index'
+  # # read one party
+  # get '/parties/:id', to: 'parties#show'
+  # # create one party
+  # get '/parties/new', to: 'parties#new' #as:thenameofyouwant
+  # post '/parties/new', to: 'parties#create' #as:thenameofyouwant
 
-  # update one party
-  get '/parties/:id/edit', to: 'parties#edit' #as:thenameofyouwant
-  patch '/parties/:id', to: 'parties/update'
+  # # update one party
+  # get '/parties/:id/edit', to: 'parties#edit' #as:thenameofyouwant
+  # patch '/parties/:id', to: 'parties/update'
 
-  # delete one party
-  delete '/parties/:id', to: 'parties#destroy' #as:thenameofyouwant
+  # # delete one party
+  # delete '/parties/:id', to: 'parties#destroy' #as:thenameofyouwant
 
 
   # CREATING CRUD FOR DASHBOARD
